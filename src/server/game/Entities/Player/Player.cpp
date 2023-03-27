d93ea0600a3fc73501edede1cdad19a33c92f07e
dio85 <k1ng@freemail.hu>
2023-03-27 07:41:03 +0200

    Update Player.cpp

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -15389,7 +15389,7 @@ void Player::RewardQuest(Quest const* quest, LootItemType rewardType, uint32 rew
     //lets remove flag for delayed teleports
     SetCanDelayTeleport(false);
 
-    bool canHaveNextQuest = !quest->HasFlag(QUEST_FLAGS_AUTOCOMPLETE) ? !questGiver->IsPlayer() : true;
+    bool canHaveNextQuest = !quest->HasFlag(QUEST_FLAGS_AUTOCOMPLETE) ? questGiver && !questGiver->IsPlayer() : true;
     if (canHaveNextQuest)
     {
         switch (questGiver->GetTypeId())
@@ -29621,7 +29621,7 @@ void Player::CreateChallengeKey(Item* item)
 
 void Player::ResetChallengeKey()
 {
-    DestroyItemCount(138019, 100, true, true);
+    DestroyItemCount(180653, 100, true, true);
 
     if (!m_challengeKeyInfo.IsActive())
         return;
